1036a3061393e1732d3f1daa5bc68b81cd07ed18
Abiola Oyewale <abiol4001@gmail.com>
2023-11-26 15:38:42 +0100

    Fixing deployment issue

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,14 +1,17 @@
 "use client"
 import { useRouter } from 'next/navigation'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 type Props = {}
 
 const LandingPage = (props: Props) => {
     const router = useRouter()
 
-    //Redirecting to the note page since the landing page doesnt have any content
-    router.push("/notes")
+    useEffect(()=> {
+      //Redirecting to the note page since the landing page doesnt have any content
+      return router.push("/notes")
+    },[])
+
   return (
     <div className='flex justify-center items-center h-full'>Landing Page</div>
   )
